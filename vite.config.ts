02c5241b97f184308a06d1bdf9e4391e981e1ff5
Xedll <97981835+Xedll@users.noreply.github.com>
2025-05-03 22:16:48 +0300

    trash4

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), keycloakify({
-   accountThemeImplementation: "Single-Page"
+   accountThemeImplementation: "Multi-Page"
 }),tailwindcss(),],
 })
