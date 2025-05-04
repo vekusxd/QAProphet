6b7dec819d2c4bd32e7ef329a9f57917395be0b8
Xedll <97981835+Xedll@users.noreply.github.com>
2025-05-04 16:43:04 +0300

    account pages

--- src/keycloak-theme/account/KcPage.tsx
+++ src/keycloak-theme/account/KcPage.tsx
@@ -4,22 +4,23 @@ import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/account/DefaultPage";
 import Template from "keycloakify/account/Template";
+import './main.css'
 
 export default function KcPage(props: { kcContext: KcContext }) {
-    const { kcContext } = props;
+   const { kcContext } = props;
 
-    const { i18n } = useI18n({ kcContext });
+   const { i18n } = useI18n({ kcContext });
 
-    return (
-        <Suspense>
-            {(() => {
-                switch (kcContext.pageId) {
-                    default:
-                        return <DefaultPage kcContext={kcContext} i18n={i18n} classes={classes} Template={Template} doUseDefaultCss={true} />;
-                }
-            })()}
-        </Suspense>
-    );
+   return (
+      <Suspense>
+         {(() => {
+            switch (kcContext.pageId) {
+               default:
+                  return <DefaultPage kcContext={kcContext} i18n={i18n} classes={classes} Template={Template} doUseDefaultCss={true} />;
+            }
+         })()}
+      </Suspense>
+   );
 }
 
 const classes = {} satisfies { [key in ClassKey]?: string };
